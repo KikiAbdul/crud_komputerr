38246908a0402640868f67abff84bd5c12ea4cf3
Yusufmu <maulanayusuf@smkwikrama.sch.id>
2022-12-06 13:23:00 +0700

    menambahkan animasi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,13 @@
             <div class="img">
                 <img class="rounded float-end" src="assets/img/imgSMK.png" alt="logoSMk" width="50px">
             </div>
-            <div class="image" style="">
+            <div class="image">
                 <img class="rounded mx-auto d-block" src="assets/img/logoRPL.png" alt="Logo RPL" width="150px">
                 <h5 class="text-center">Daftar Penempatan Komputer PPLG</h5>
                 <hr style="width: 300px" class="rounded mx-auto d-block">
             </div>
             <a class="btn btn-light mt-3 bi bi-arrow-left" style="width: 100px" href="/komputer">Back</a>
-            <div class="row mt-5">
+            <div class="row mt-5"   data-aos="fade-down">
                 @foreach ($komputers as $item)
                     <div class="col-4">
                         <div class="card shadow p-0 mb-5 bg-body rounded" style="width: 18rem;">
